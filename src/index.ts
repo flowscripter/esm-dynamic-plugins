ff240a9886b1495b78209b5bb6f0b2a1769b2f51
vectronic <hello.vectronic@gmail.com>
2019-07-23 12:13:42 +0100

    fix: relative import paths specified

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { default as ExtensionDescriptor } from 'api/ExtensionDescriptor';
-export { default as ExtensionInfo } from 'api/ExtensionInfo';
-export { default as ExtensionFactory } from 'api/ExtensionFactory';
-export { default as Plugin } from 'api/Plugin';
-export { default as PluginManager } from 'api/PluginManager';
-export { default as BrowserPluginManager } from 'core/BrowserPluginManager';
-export { default as NodePluginManager } from 'core/NodePluginManager';
+export { default as ExtensionDescriptor } from './api/ExtensionDescriptor';
+export { default as ExtensionInfo } from './api/ExtensionInfo';
+export { default as ExtensionFactory } from './api/ExtensionFactory';
+export { default as Plugin } from './api/Plugin';
+export { default as PluginManager } from './api/PluginManager';
+export { default as BrowserPluginManager } from './core/BrowserPluginManager';
+export { default as NodePluginManager } from './core/NodePluginManager';
